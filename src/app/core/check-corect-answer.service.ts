d67a8339607d9dfe38648ea345af22dd8ab6d0ea
Your Name <mateusz.maslanka1@outlook.com>
2019-07-19 09:33:36 +0200

    cheanges

--- src/app/core/check-corect-answer.service.ts
+++ src/app/core/check-corect-answer.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {isNullOrUndefined} from 'util';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,47 @@ export class CheckCorectAnswerService {
   constructor() { }
 
   private result = 0;
-
   private isAnswerList = [];
+  private listForLastAnswere = [];
+  private getAnswer;
 
   checkAnswer(questionsForShowFromQuiz, answerFromQuiz): boolean {
-      console.log(questionsForShowFromQuiz.value, answerFromQuiz);
-      const idUser = questionsForShowFromQuiz.value.id;
+      // console.log(questionsForShowFromQuiz.value, answerFromQuiz);
+      const idQuestion = questionsForShowFromQuiz.value.id;
+
       this.isAnswerList.push({
-        idUser
-      })
-      console.log(this.isAnswerList);
+         idQuestion,
+         answerFromQuiz
+       });
+
+     //  this.isAnswerList.forEach((element, index) => {
+     //     if (this.isAnswerList[index].idQuestion === questionsForShowFromQuiz.value.id) {
+     //        this.isAnswerList[index].idQuestion = element;
+     //        console.log(element);
+     //     }
+     // });
+     //
+     //  console.log(this.isAnswerList);
+
+      this.isAnswerList.forEach((element, index) => {
+           if (element.idQuestion === questionsForShowFromQuiz.value.id) {
+             this.getAnswer = element
+             this.isAnswerList.splice(index, 1);
+           }
+       });
+
+      // console.log(this.getAnswer);
+      this.isAnswerList.push(
+        this.getAnswer
+      );
+      //console.log(this.isAnswerList);
+
+      this.isAnswerList.forEach((element, index) => {
+        if (element.idQuestion === questionsForShowFromQuiz.value.id) {
+            console.log(element);
+        }
+        });
+
       return +answerFromQuiz === questionsForShowFromQuiz.value.correctAnswer;
   }
 
