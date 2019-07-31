e8c77c5475754acf2032be207d4f49a9b3801039
Your Name <mateusz.maslanka1@outlook.com>
2019-07-31 08:53:31 +0200

    cheanges

--- src/app/core/go-to-question-without-answer.service.ts
+++ src/app/core/go-to-question-without-answer.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
 import {isNull} from 'util';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoToQuestionWithoutAnswerService {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   private getIndex = null;
   private counter = 0;
 
   goToQuestion(tabWithQuestion, indexTabWithQuestion) {
       for (let i = indexTabWithQuestion; i < tabWithQuestion.length; i++) {
-         if (isNull(tabWithQuestion[i].value.userAnswer) && this.counter === 0) {
+        if (isNull(tabWithQuestion[i].value.userAnswer) && this.counter === 0) {
            this.getIndex = i;
            this.counter++;
+           break;
            } else if (tabWithQuestion[i].value.userAnswer !== null) {
-             this.getIndex = null;
+            this.goSummary();
+            this.getIndex = null;
            }
         }
       this.counter = 0;
       return this.getIndex;
+  }
+
+  goSummary() {
+    this.router.navigate([`../summary`]);
   }
 }
