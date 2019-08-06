848a49d0f859efaecc37bc9bc7ae010f7e4190b3
Your Name <mateusz.maslanka1@outlook.com>
2019-08-06 12:42:30 +0200

    cheanges

--- src/app/core/add-question/add-question.component.ts
+++ src/app/core/add-question/add-question.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ConnectToJsonServerService} from '../connect-to-json-server.service';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-question',
@@ -30,16 +31,23 @@ export class AddQuestionComponent implements OnInit {
   }
 
   addQuestionToJsonServer() {
-    this.ObjWithQuestion = {
-        question: this.question,
-        answers:  this.answers,
-        correctAnswer: this.correctAnswer,
-        userAnswer: null
-    };
-    this.connectToJsonServerService.sendNewQuestionToJsonServer(this.ObjWithQuestion);
-    this.answers = [];
-    this.question = null;
-    this.correctAnswer = null;
-    this.num = 1;
+    swal.fire({
+      title: 'Czy napewno chesz zakończyć', text: 'kliknij na przycisk', type: 'warning',
+      showCancelButton: true, cancelButtonColor: '#d33'
+    }).then((result) => {
+       if (result.value) {
+        this.ObjWithQuestion = {
+         question: this.question,
+         answers:  this.answers,
+         correctAnswer: this.correctAnswer,
+         userAnswer: null
+        };
+        this.connectToJsonServerService.sendNewQuestionToJsonServer(this.ObjWithQuestion);
+        this.answers = [];
+        this.question = null;
+        this.correctAnswer = null;
+        this.num = 1;
+      }
+    });
   }
 }
