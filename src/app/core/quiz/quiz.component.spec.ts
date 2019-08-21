0c142a4853272978ff1317e7875941e0ad5a1659
Your Name <mateusz.maslanka1@outlook.com>
2019-08-21 09:59:21 +0200

    cheanges

--- src/app/core/quiz/quiz.component.spec.ts
+++ src/app/core/quiz/quiz.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 
 import {QuizComponent} from './quiz.component';
 import {CUSTOM_ELEMENTS_SCHEMA, DebugElement, NO_ERRORS_SCHEMA} from '@angular/core';
@@ -7,7 +7,7 @@ import {CheckCorrectAnswerService} from '../check-correct-answer.service';
 import {GoToQuestionWithoutAnswerService} from '../go-to-question-without-answer.service';
 import {TimeService} from '../time.service';
 import {Router} from '@angular/router';
-import {of, Subject} from 'rxjs';
+import {of} from 'rxjs';
 import {By} from '@angular/platform-browser';
 
 describe('QuizComponent', () => {
@@ -48,7 +48,7 @@ describe('QuizComponent', () => {
 
   const router = {
     navigate: jasmine.createSpy('navigate'),
-    // url: '/quiz/0',
+    url: '/quiz/0',
   };
 
   beforeEach(async(() => {
@@ -74,29 +74,43 @@ describe('QuizComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+     expect(component).toBeTruthy();
+   });
 
   it('check all data', () => {
-    component.answer = '2';
-    fixture.detectChanges();
-    component.ngOnInit();
-    fixture.detectChanges();
-    expect(router.navigate).toHaveBeenCalledWith(['/quiz/0']);
-    expect(component.indexForNextQuestion).toBe(0);
-    expect(component.questionsForShow).toBe(component.questionsForShow);
-    expect(component.answer).toBe('2');
-  });
+     component.answer = '2';
+     fixture.detectChanges();
+     component.ngOnInit();
+     fixture.detectChanges();
+     expect(router.navigate).toHaveBeenCalledWith([`/quiz/${component.indexForNextQuestion}`]);
+     expect(component.indexForNextQuestion).toBe(0);
+     expect(component.questionsForShow).toBe(component.questionsForShow);
+     expect(component.answer).toBe('2');
+   });
 
   it('should next question', fakeAsync(() => {
-    tick(15000);
+     timeService.time$.subscribe(data => {
+       const debugElement: DebugElement = fixture.debugElement;
+       const buttonNextEL = debugElement.query(By.css('.button-next-pervious:nth-of-type(2)')).nativeElement;
+       component.answer = '2';
+       buttonNextEL.disabled = false;
+       buttonNextEL.click();
+       expect(component.indexForNextQuestion).toBe(1);
+       expect(component.answer).toBe(component.answer);
+       // expect(router.navigate).toHaveBeenCalledWith([`/quiz/${component.indexForNextQuestion}`]);
+     });
+   }));
+
+  it('should previous question', fakeAsync(() => {
     timeService.time$.subscribe(data => {
       const debugElement: DebugElement = fixture.debugElement;
-      const buttonNextEL = debugElement.query(By.css('.button-next-pervious:nth-of-type(2)')).nativeElement;
-      buttonNextEL.disabled = false;
-      buttonNextEL.click();
-      expect(component.indexForNextQuestion).toBe(1);
-     // expect(router.navigate).toHaveBeenCalledWith([`quiz/1`]);
+      const buttonPreviousEL = debugElement.query(By.css('.button-next-pervious')).nativeElement;
+      component.answer = '2';
+      component.indexForNextQuestion = 1;
+      buttonPreviousEL.disabled = false;
+      buttonPreviousEL.click();
+      expect(component.indexForNextQuestion).toBe(0);
+      expect(component.answer).toBe(component.answer);
     });
   }));
 });
